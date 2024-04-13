318a32c7a24a370b9d64e7f6ef22960917bc3a5d
tigerchris2000 <tigerchris2000@gmail.com>
2024-04-13 13:50:53 +0200

    begin

---
+++ Godot/Code/Player.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+@export var max_speed: float = 10
+@export var acc: float = 10
+@export var dec: float = 10
+
+var move = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# apply_force()
+	pass
