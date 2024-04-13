7c92a49ab20973d0f87071cf39bd9745e6885551
Christopher Thomas <51336637+tigerchris2000@users.noreply.github.com>
2024-04-13 16:41:20 +0200

    cloud7

--- Godot/Code/Player.gd
+++ Godot/Code/Player.gd
@@ -5,9 +5,12 @@ extends RigidBody2D
 @export var dec: float = 10
 @export var jump_height: float = 10
 @export var grav: float = 100
+@export var offset: float = 200
 
 var move: int = 0
 var grounded = true
+
+var cloud = preload("res://Presets/Abilities/cloud.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,24 +24,7 @@ func _integrate_forces(state):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("dev"):
-		grounded = true
-		print(grounded)
-		
-	if Input.is_action_pressed("Dev_Add"):
-		grav += 1
-		print(grav)
-		# max_speed += 10
-		# print(max_speed)
-		# linear_damp += 0.5
-		# print(linear_damp)
-	if Input.is_action_pressed("Dev_Sub"):
-		grav -= 1
-		print(grav)
-		# max_speed -= 10
-		# print(max_speed)
-		# linear_damp -= 0.5
-		# print(linear_damp)
+	grounded = $RayCast2D.is_colliding()
 		
 	if Input.is_action_pressed("Left"):
 		move = -1
@@ -47,17 +33,19 @@ func _process(delta):
 	else:
 		move = 0
 
-	
 	apply_force(Vector2.RIGHT * move * acc * delta)
 	
 	if Input.is_action_pressed("Jump") and grounded:
 		grounded = false
 		apply_central_impulse(Vector2(0,-1) * jump_height)
 		
+	if Input.is_action_just_pressed("dev"):
+		spawn(cloud, position+ Vector2.DOWN * offset)
+		
 	if not grounded:
 		apply_central_impulse(Vector2.DOWN * grav * delta)
-		
-	print(linear_velocity.y)
 
-func is_grounded() -> bool:
-	return true
+func spawn(abilty, pos):
+	var a = abilty.instantiate()
+	a.position = pos
+	add_child(a)
